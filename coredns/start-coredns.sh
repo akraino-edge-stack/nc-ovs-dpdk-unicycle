a6201a6ebb26ab11b451e0f9dffdb5848491ad69
Georg Kunz <georg.kunz@ericsson.com>
2019-11-04 20:41:29 +0000

    CoreDNS configuration for Airship 1.3
    Newer versions of Airship require specific hostnames to be
    registered in DNS. This adds the configuration files for a coredns
    deployment in the Ericsson lab.
    
    Change-Id: Id14c160db542d51ea6962ffe2198714fdeb9238f

---
+++ coredns/start-coredns.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run --detach --restart=always --name coredns --network=host --volume /opt/akraino/ovs-dpdk-unicycle/coredns:/etc/coredns coredns/coredns:1.3.1 -conf /etc/coredns/Corefile
